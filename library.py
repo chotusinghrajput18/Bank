5b3850fed5a98472d378faccd90f107788c107c5
chotusinghrajput18 <chotusinghrajput57@gmail.com>
2026-09-24 21:38:56 +0530

    adding new functions

--- library.py
+++ library.py
@@ -2,5 +2,14 @@ class Library:
     def __init__(self):
         self.books = []
     def addBook(self):
-        try:
-            new_book=input("enter book for adding: ")
+        book=input("enter book to add: ")
+        if book not in self.books:
+            self.books.append(book)
+        else:
+            print("Book already exists")
+    def removeBook(self):
+        book=input("enter book to remove: ")
+        if book in self.books:
+            self.books.remove(book)
+        else:
+            print("Book not found") 

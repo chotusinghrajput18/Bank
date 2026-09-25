b29d644da2608f321e066b19d3a32c5c907d2645
chotusinghrajput18 <chotusinghrajput57@gmail.com>
2026-09-25 21:12:03 +0530

    adding functions

--- .py
+++ .py
@@ -1,23 +1,33 @@
-class practice:
+class Practice:
     n=1
     def __init__(self):
         print("you are now eligible for registration.")
         self.dashboard()
     def dashboard(self):
         print("1- Registration")
+        ch=int(input("Enter a choice: "))
+        if ch==1:
+            self.registration()
     def registration(self):
         while self.n!=0:
             name=input("Enter your name: ")
             if name.isalpha():
-                phone=int(input("Enter phone no.: " ))
-                if len(phone)==10:
-                    email=input("Enter your email: ")
-                    if email.isalnum():
-                        pass
+                while self.n!=0:
+                    phone=input("Enter phone no.: " )
+                    if len(phone)==10 and phone.isalnum():
+                        while self.n!=0:
+                            email=input("Enter your email: ")
+                            if '@' in email and len(email)>8:
+                                print("Registration successfull ")
+                                self.n=0
+                                break
+                            else:
+                                print("invalid input")
+                                continue
                     else:
+                        print("invalid phone")
                         continue
                 else:
+                    print("Invalid name")
                     continue
-            else:
-                continue
-            
+p=Practice()
